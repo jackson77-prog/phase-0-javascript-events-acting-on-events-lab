f4f8cf4fe17e850111f69ed3bd39b8e9da153e9c
Jackson <jackson.njihia@student.moringaschool.com>
2024-01-17 03:01:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  const gameWidth = document.getElementById("game").offsetWidth;
+
+  if (left < gameWidth - dodger.offsetWidth) {
+    dodger.style.left = `${left + 1}px`;
+  } else {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
